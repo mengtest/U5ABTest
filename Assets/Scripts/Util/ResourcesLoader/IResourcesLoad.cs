84da7a0e7315aff83a95eda8ce25dbef98d1ca99
resetOTER <352070977@qq.com>
2016-02-28 15:15:56 +0800

    初次提交库

---
+++ Assets/Scripts/Util/ResourcesLoader/IResourcesLoad.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+interface IResourcesLoad {
+
+    //加载资源
+    UnityEngine.Object LoadResource(string objectName, Action afterLoadAct, Action<int> progressAct = null);
+    //加载资源们
+    UnityEngine.Object[] LoadResources(string[] objectName, Action afterLoadAct, Action<int> progressAct = null);
+    //加载资源并实例化
+    UnityEngine.Object LoadAndGetInstance(string[] objectName, Action afterLoadAct, Action<int> progressAct = null);
+
+}
