c9ebd1c4443bc629fc12bb581f68c782f257da82
董宸 <352070977@qq.com>
2016-05-06 00:22:56 +0800

    建立NGUIManager以及BaseUI

---
+++ Assets/ResetCore/NGUI/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using ResetCore.Util;
+
+namespace ResetCore.NGUI
+{
+    public class UIManager : MonoSingleton<UIManager>
+    {
+        [SerializeField]
+        private Camera camera;
+        [SerializeField]
+        private Transform normalUI;
+        [SerializeField]
+        private Transform popupUI;
+        [SerializeField]
+        private Transform topUI;
+        
+
+    }
+}
+
