0a96428704a19d76b78680211ff53323c16570e2
resetOTER <352070977@qq.com>
2016-05-08 03:38:17 +0800

    1、增强特效池模块 2、增加场景加载管理模块

---
+++ Assets/ResetCore/Util/LoadSceneManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace ResetCore.Util
+{
+    public class LoadSceneManager : Singleton<LoadSceneManager>
+    {
+        public bool isLoading = false;
+        public void LoadScene(string sceneName, System.Action<bool> loadedAct = null, System.Action<float> progressAct = null)
+        {
+            if (isLoading == false)
+            {
+                CoroutineTaskManager.Instance.AddTask(
+                new CoroutineTaskManager.CoroutineTask("LoadScene" + sceneName, DoLoadScene(sceneName), loadedAct));
+                CoroutineTaskManager.Instance.AddTask(
+                    new CoroutineTaskManager.CoroutineTask("LoadSceneProgress" + sceneName, DoLoadSceneProgress(progressAct), loadedAct));
+            }
+            else
+            {
+                Debug.logger.LogError("加载场景错误", "正在加载其他场景无法加载新场景");
+            }
+        }
+
+        private AsyncOperation operation;
+        IEnumerator DoLoadScene(string sceneName)
+        {
+            isLoading = true;
+            yield return operation = SceneManager.LoadSceneAsync(sceneName);
+            isLoading = false;
+        }
+
+        IEnumerator DoLoadSceneProgress(System.Action<float> progressAct)
+        {
+            while (!operation.isDone)
+            {
+                if (progressAct != null)
+                {
+                    progressAct(operation.progress);
+                }
+                yield return null;
+            }
+        }  
+	
+	}
+
+}
+
--- Assets/ResetCore/Util/ObjectPool/EffectPool.cs
+++ Assets/ResetCore/Util/ObjectPool/EffectPool.cs
@@ -17,31 +17,50 @@ namespace ResetCore.Util
             EfPool = new List<GameObject>();
         }
 
-        public GameObject PlayEffectInRoot(string efName, Vector3 pos)
+        public GameObject PlayEffectInRoot(string efName, Vector3 pos, float time = -1)
         {
             GameObject efGo = FindOrCreateObject(efName);
             efGo.transform.position = pos;
+            efGo.SetActive(true);
             Play(efGo);
+            if (time > 0)
+            {
+                CoroutineTaskManager.Instance.WaitSecondTodo(() =>
+                {
+                    HideEffect(efGo);
+                }, time);
+            }
+
             return efGo;
         }
 
-        public GameObject PlayEffectUnderTran(string efName, Transform tran, Vector3 localPos)
+        public GameObject PlayEffectUnderTran(string efName, Transform tran, Vector3 localPos, float time = -1)
         {
             GameObject efGo = FindOrCreateObject(efName);
             efGo.transform.parent = tran;
             efGo.transform.localPosition = localPos;
+            efGo.SetActive(true);
+            if (time > 0)
+            {
+                CoroutineTaskManager.Instance.WaitSecondTodo(() =>
+                {
+                    HideEffect(efGo);
+                }, time);
+            }
             Play(efGo);
             return efGo;
         }
 
         public void HideEffect(GameObject go)
         {
+            if (go == null) return;
             if (!EfPool.Contains(go))
             {
                 Debug.logger.LogWarning("隐藏特效", "特效" + go.name + "不属于特效池");
                 return;
             }
             go.SetActive(false);
+            go.transform.SetParent(transform);
         }
 
         private GameObject FindOrCreateObject(string efName)
@@ -58,8 +77,14 @@ namespace ResetCore.Util
 
         private GameObject CheckPool(string efName)
         {
-            foreach (GameObject go in EfPool)
+            for (int i = 0; i < EfPool.Count; i++)
             {
+                GameObject go = EfPool[i];
+                if (go == null)
+                {
+                    EfPool.Remove(go);
+                    continue;
+                }
                 string goName = go.name;
                 if (goName.Contains("(Clone)"))
                 {
