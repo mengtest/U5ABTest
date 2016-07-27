55aa6b82fc1a8ea814796028c687867643e59679
resetOTER <352070977@qq.com>
2016-07-28 03:33:10 +0800

    修复单例

--- Assets/ResetCore/Util/Singleton.cs
+++ Assets/ResetCore/Util/Singleton.cs
@@ -8,13 +8,7 @@ namespace ResetCore.Util
 {
     public abstract class Singleton<T> where T : Singleton<T>, new()
     {
-        protected Singleton()
-        {
-            if(typeof(T).GetConstructors(BindingFlags.Instance|BindingFlags.NonPublic).Length == 0)
-            {
-                throw new Exception("单例模式不允许使用公共构造函数构造");
-            }
-        }
+
         private static T Instance = null;
         public static T instance
         {
