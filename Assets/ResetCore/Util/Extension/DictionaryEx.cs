b5aaecc577bf2e672f9f09c58e29074ce3da7df8
董宸 <352070977@qq.com>
2016-08-08 18:11:56 +0800

    扩展Dictionary以及List的随机功能

---
+++ Assets/ResetCore/Util/Extension/DictionaryEx.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class DictionaryEx {
+
+    /// <summary>
+    /// 根据权值获取值，Key为值，Value为权值
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="powersDict"></param>
+    /// <returns></returns>
+    public static T GetRandomWithPower<T>(this Dictionary<T, int> powersDict)
+    {
+        List<T> keys = new List<T>();
+        List<int> values = new List<int>();
+        foreach (T key in powersDict.Keys)
+        {
+            keys.Add(key);
+            values.Add(powersDict[key]);
+        }
+        int finalKeyIndex = values.GetRandomWithPower();
+        return keys[finalKeyIndex];
+    }
+
+
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+    public int a = 1;
+}
